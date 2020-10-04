0bd3c7ded8ede6fe1f8bebb1f597bfebbef7c5c7
andy.boot <bootandy@gmail.com>
2020-10-04 11:56:29 +0100

    [i3blocks] fix disk script
    remove useless echo

--- i3blocks/scripts/disk.sh
+++ i3blocks/scripts/disk.sh
@@ -6,7 +6,7 @@ df -kh | grep nvme | head -1 | awk '{print $3, $5}'
 #v=`df -kh | grep ubuntu | awk '{print $5}'`
 v=`df -kh | grep nvme | head -1 | awk '{print $5}' | sed 's/%//'`
 
-echo $v
+#echo $v
 if [ "$((v))" -gt 80 ]; then
 	exit 33
 fi
