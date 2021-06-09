bcf693178e3123a42cd7eab1dc95cb0c67baf876
andy.boot <bootandy@gmail.com>
2021-06-09 12:22:34 +0100

    Add cpu temp to i3 status bar, change i3 lock key

--- i3blocks/scripts/weather.sh
+++ i3blocks/scripts/weather.sh
@@ -1,4 +1,4 @@
-echo -n 'Temp: '
+echo -n ': '
 curl -silent https://weather-broker-cdn.api.bbci.co.uk/en/observation/rss/2643743 > /tmp/weather2.html
 egrep "Temperature.* \(" /tmp/weather2.html -o | cut -d ' ' -f 2
 
